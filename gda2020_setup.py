dd04bd7ad249a8a284805bc5ccf4343e0ff93583
KyranCookGA <kyran.cook@ga.gov.au>
2026-07-30 01:45:10 +0000

    Renamed some files to show how file naming could work

---
+++ gda2020_setup.py
@@ -0,0 +1,8 @@
+# This script is used to setup the file strucutre and files needed to complete each step of the GDA2020 adjustment
+#
+# It is split into 4 areas that can be set up individually or all together
+# 1. APREF
+# 2. NGCA
+# 3. NADJ
+# 4. QAQC
+
