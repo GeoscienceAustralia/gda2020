6d5f77fd758eb0105c78e2f37e76dceb081847d5
KyranCookGA <kyran.cook@ga.gov.au>
2026-08-06 03:40:40 +0000

    Fixed import statements in APREF

--- apref/scripts/apref_comparisonReport.py
+++ apref/scripts/apref_comparisonReport.py
@@ -9,8 +9,8 @@ import pandas as pd
 import math
 import argparse
 
-from gda2020.APREF.scripts.apref_comparisonQAQC import check_solutions, check_disconts, check_epochs, check_pos
-from gda2020.APREF.scripts.apref_comparisonMap import make_map
+from gda2020.apref.scripts.apref_comparisonQAQC import check_solutions, check_disconts, check_epochs, check_pos
+from gda2020.apref.scripts.apref_comparisonMap import make_map
 
 """
 This script will create a transition report between different apref versions. 
