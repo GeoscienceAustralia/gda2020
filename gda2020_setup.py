eca3b01e25cdc015ac5789065a42330ee99c41b4
Kyran-Cook <kyran.cook@ga.gov.au>
2026-08-24 23:43:17 +0000

    Removed directory changes

--- gda2020_setup.py
+++ gda2020_setup.py
@@ -224,17 +224,19 @@ def setup_processes(repo_root, processes, dry_run=False):
 
     # Downlad packages needed for rest of setup to run.
     for package in ["zip", "unzip"]:
-        subprocess.run(["sudo", "apt", "install", package], check=True)
+        subprocess.run(["sudo", "apt", "install", "-y", package], check=True)
         print(f"  Downloaded {package}")
 
     if shutil.which("aws") is None:
-        os.chdir(os.path.expanduser("~"))
-        subprocess.run(["curl", "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip", "-o", "awscliv2.zip"], check=True) # Using a link here is a bit dangerous but it is what aws recommends https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
-        subprocess.run(["unzip", "awscliv2.zip"], check=True)
-        os.remove("awscliv2.zip")
-        subprocess.run(["sudo", "aws/install"])
+        
+        home_dir = os.path.expanduser("~")
+        zip_file = os.path.join(home_dir, "awscliv2.zip")
+        
+        subprocess.run(["curl", "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip", "-o", zip_file], check=True) # Using a link here is a bit dangerous but it is what aws recommends https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+        subprocess.run(["unzip", zip_file], check=True)
+        os.remove(zip_file)
+        subprocess.run(["sudo", os.path.join(home_dir, "aws", "install")], check=True)
         print("  Downloaded AWS CLI")
-        os.chdir(repo_root)
     else:
         print("  AWS CLI already downloaded")
 
