1abee711874c3dc679491f91c0e41cd69a318db4
Kyran-Cook <kyran.cook@ga.gov.au>
2026-08-27 05:23:55 +0000

    Removed need to enter archive date

--- ngca/run_NGCA_PRE_PROCESSING.sh
+++ ngca/run_NGCA_PRE_PROCESSING.sh
@@ -5,9 +5,6 @@ if [[ -d "$SCRIPT_DIR/scripts" ]] && [[ ":$PATH:" != *":$SCRIPT_DIR/scripts:"* ]
     export PATH="$PATH:$SCRIPT_DIR/scripts"
 fi
 
-# Archive date for NGCA processing, e.g. the date on which get_ngca.py has downloaded the NGCA archive contents for the jurisdictions being processed 
-ARCHIVE=20260827
-
 # List of jurisdictions to run consecutively - increasing number of RINEX
 #JURIS_LIST=("tas" "act" "vic" "sa" "nt" "wa" "nsw" "qld")
 
